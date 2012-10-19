ae0ec546301be36cd2aa9710c67ac8cadd3d8fac
John Ahlroos <john.ahlroos@jasoft.fi>
2012-10-19 20:44:32 +0300

    Task descriptions and Copyright notices

--- src/main/groovy/fi/jasoft/plugin/CreateComponentTask.groovy
+++ src/main/groovy/fi/jasoft/plugin/CreateComponentTask.groovy
@@ -1,3 +1,18 @@
+/*
+* Copyright 2012 John Ahlroos
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package fi.jasoft.plugin;
 
 import org.gradle.api.DefaultTask;
@@ -5,6 +20,10 @@ import org.gradle.api.tasks.TaskAction;
 import fi.jasoft.plugin.ui.TemplateUtil;
 
 class CreateComponentTask extends DefaultTask {
+
+    public CreateComponentTask(){
+        description = "Creates a new Vaadin Component."
+    }
 
  	@TaskAction
     public void run() {
