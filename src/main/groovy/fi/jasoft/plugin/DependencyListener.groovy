3cd75dd840feeb97da00bfea9451320f92333039
John Ahlroos <john.ahlroos@jasoft.fi>
2013-04-20 20:16:53 +0300

    Ensure Jetty provided servlet 3 api dependency gets onto the project classpath. #36

--- src/main/groovy/fi/jasoft/plugin/DependencyListener.groovy
+++ src/main/groovy/fi/jasoft/plugin/DependencyListener.groovy
@@ -37,84 +37,115 @@ class DependencyListener implements ProjectEvaluationListener{
 		project.repositories.mavenCentral()
         project.repositories.mavenLocal()
 
-		if(project.repositories.findByName('Vaadin addons') == null) {
-			project.repositories.mavenRepo(name: 'Vaadin addons', url: 'http://maven.vaadin.com/vaadin-addons')
-		}
+        addRepositories(project)
 
-		if(project.repositories.findByName('Jasoft.fi Maven repository') == null) {
-			project.repositories.mavenRepo(name: 'Jasoft.fi Maven repository', url: 'http://mvn.jasoft.fi/maven2')
-		}
-
-        project.repositories.flatDir(dirs: '/home/johnnie/Repositories/gradle-vaadin-plugin/build/libs')
-
-		// Configurations
-		if(!project.configurations.hasProperty('vaadin')){
-			project.configurations.add('vaadin')
-			project.sourceSets.main.compileClasspath += project.configurations.vaadin
-			project.sourceSets.test.compileClasspath += project.configurations.vaadin
-			project.sourceSets.test.runtimeClasspath += project.configurations.vaadin
-			project.war.classpath(project.configurations.vaadin)	
-		}
-
-        if(!project.configurations.hasProperty('vaadinSources')){
-            project.configurations.add('vaadinSources')
-        }
-
-		if(!project.configurations.hasProperty('gwt')){
-			project.configurations.add('gwt')
-			project.sourceSets.main.compileClasspath += project.configurations.gwt
-			project.sourceSets.test.compileClasspath += project.configurations.gwt
-			project.sourceSets.test.runtimeClasspath += project.configurations.gwt
-		}
-
-        if(!project.configurations.hasProperty("gwtSources")){
-            project.configurations.add('gwtSources')
-        }
-
-		if(!project.configurations.hasProperty('jetty8')){
-			project.configurations.add("jetty8")	
-		}
-		
-		// Tasks
-        project.dependencies.add('jetty8', 'org.eclipse.jetty.aggregate:jetty-all-server:8.1.10.v20130312')
-        project.dependencies.add('jetty8', 'fi.jasoft.plugin:VaadinPlugin:'+VaadinPlugin.getVersion())
-        project.dependencies.add('jetty8', 'asm:asm-all:3.3.1')
+        createJetty8Configuration(project)
 
 		def version = project.vaadin.version
 		def gwtVersion = project.vaadin.gwt.version
 		if(version.startsWith("6")){
-			project.dependencies.add("vaadin", "com.vaadin:vaadin:${version}")
-			if(project.vaadin.widgetset != null){
-				project.dependencies.add("gwt", "com.google.gwt:gwt-user:"+gwtVersion)
-				project.dependencies.add("gwt", "com.google.gwt:gwt-dev:"+gwtVersion)
-				project.dependencies.add("gwt",	"javax.validation:validation-api:1.0.0.GA")
-				project.dependencies.add("gwt",	"javax.validation:validation-api:1.0.0.GA:sources")
-			}
+			createVaadin6Configuration(project, version, gwtVersion)
 		} else{ 
-			File webAppDir = project.convention.getPlugin(WarPluginConvention).webAppDir
-    		FileTree themes = project.fileTree(dir: webAppDir.canonicalPath + '/VAADIN/themes', include: '**/styles.scss')
-			if(!themes.isEmpty()){
-				project.dependencies.add("vaadin",	"com.vaadin:vaadin-theme-compiler:${version}")
-                project.dependencies.add("vaadinSources",	"com.vaadin:vaadin-theme-compiler:${version}:sources" )
-            }
-
-			if(project.vaadin.widgetset == null){
-				project.dependencies.add("vaadin",	"com.vaadin:vaadin-client-compiled:${version}")
-			} else {
-				project.dependencies.add("gwt",	"com.vaadin:vaadin-client-compiler:${version}")
-                project.dependencies.add("gwtSources",	"com.vaadin:vaadin-client-compiler:${version}:sources")
-
-				project.dependencies.add("gwt",	"com.vaadin:vaadin-client:${version}")
-                project.dependencies.add("gwtSources",	"com.vaadin:vaadin-client:${version}:sources")
-
-				project.dependencies.add("gwt",	"javax.validation:validation-api:1.0.0.GA")
-				project.dependencies.add("gwt",	"javax.validation:validation-api:1.0.0.GA:sources")
-			}
-
-			project.dependencies.add("vaadin", "com.vaadin:vaadin-server:${version}")
-            project.dependencies.add("vaadinSources", "com.vaadin:vaadin-server:${version}:sources")
-
-			project.dependencies.add("vaadin", "com.vaadin:vaadin-themes:${version}")
+			createVaadin7Configuration(project, version)
 		}
 	}
+
+    private static void addRepositories(Project project){
+        if(project.repositories.findByName('Vaadin addons') == null) {
+            project.repositories.mavenRepo(name: 'Vaadin addons', url: 'http://maven.vaadin.com/vaadin-addons')
+        }
+
+        if(project.repositories.findByName('Jasoft.fi Maven repository') == null) {
+            project.repositories.mavenRepo(name: 'Jasoft.fi Maven repository', url: 'http://mvn.jasoft.fi/maven2')
+        }
+
+        if(new File("/home/johnnie/Repositories/gradle-vaadin-plugin/build/libs").exists()){
+            //FIXME For Development so the development plugin is found
+            project.repositories.flatDir(dirs: '/home/johnnie/Repositories/gradle-vaadin-plugin/build/libs')
+        }
+    }
+
+    private static void createJetty8Configuration(Project project){
+        if(!project.configurations.hasProperty('jetty8')){
+            project.configurations.add("jetty8")
+            project.dependencies.add('jetty8', 'org.eclipse.jetty.aggregate:jetty-all-server:8.1.10.v20130312')
+            project.dependencies.add('jetty8', 'fi.jasoft.plugin:VaadinPlugin:'+VaadinPlugin.getVersion())
+            project.dependencies.add('jetty8', 'asm:asm-all:3.3.1')
+        }
+
+        if(!project.configurations.hasProperty('vaadinSources')){
+            project.configurations.add('vaadinSources')
+        }
+    }
+
+    private static void createCommonVaadinConfiguration(Project project){
+        createGWTConfiguration(project)
+        if(!project.configurations.hasProperty('vaadin')){
+            project.configurations.add('vaadin')
+            project.sourceSets.main.compileClasspath += project.configurations.vaadin
+            project.sourceSets.test.compileClasspath += project.configurations.vaadin
+            project.sourceSets.test.runtimeClasspath += project.configurations.vaadin
+
+            // For servlet 3 support
+            project.sourceSets.main.compileClasspath += project.configurations.jetty8
+            project.sourceSets.test.compileClasspath += project.configurations.jetty8
+            project.sourceSets.test.runtimeClasspath += project.configurations.jetty8
+
+            project.war.classpath(project.configurations.vaadin)
+        }
+    }
+
+    private static void createVaadin6Configuration(Project project, String version, String gwtVersion){
+        createCommonVaadinConfiguration(project)
+
+        project.dependencies.add("vaadin", "com.vaadin:vaadin:${version}")
+        if(project.vaadin.widgetset != null){
+            project.dependencies.add("gwt", "com.google.gwt:gwt-user:"+gwtVersion)
+            project.dependencies.add("gwt", "com.google.gwt:gwt-dev:"+gwtVersion)
+            project.dependencies.add("gwt",	"javax.validation:validation-api:1.0.0.GA")
+            project.dependencies.add("gwt",	"javax.validation:validation-api:1.0.0.GA:sources")
+        }
+    }
+
+    private static void createVaadin7Configuration(Project project, String version){
+        createCommonVaadinConfiguration(project)
+
+        File webAppDir = project.convention.getPlugin(WarPluginConvention).webAppDir
+        FileTree themes = project.fileTree(dir: webAppDir.canonicalPath + '/VAADIN/themes', include: '**/styles.scss')
+        if(!themes.isEmpty()){
+            project.dependencies.add("vaadin",	"com.vaadin:vaadin-theme-compiler:${version}")
+            project.dependencies.add("vaadinSources",	"com.vaadin:vaadin-theme-compiler:${version}:sources" )
+        }
+
+        if(project.vaadin.widgetset == null){
+            project.dependencies.add("vaadin",	"com.vaadin:vaadin-client-compiled:${version}")
+        } else {
+            project.dependencies.add("gwt",	"com.vaadin:vaadin-client-compiler:${version}")
+            project.dependencies.add("gwtSources",	"com.vaadin:vaadin-client-compiler:${version}:sources")
+
+            project.dependencies.add("gwt",	"com.vaadin:vaadin-client:${version}")
+            project.dependencies.add("gwtSources",	"com.vaadin:vaadin-client:${version}:sources")
+
+            project.dependencies.add("gwt",	"javax.validation:validation-api:1.0.0.GA")
+            project.dependencies.add("gwt",	"javax.validation:validation-api:1.0.0.GA:sources")
+        }
+
+        project.dependencies.add("vaadin", "com.vaadin:vaadin-server:${version}")
+        project.dependencies.add("vaadinSources", "com.vaadin:vaadin-server:${version}:sources")
+
+        project.dependencies.add("vaadin", "com.vaadin:vaadin-themes:${version}")
+    }
+
+    private static void createGWTConfiguration(Project project){
+        if(!project.configurations.hasProperty('gwt')){
+            project.configurations.add('gwt')
+            project.sourceSets.main.compileClasspath += project.configurations.gwt
+            project.sourceSets.test.compileClasspath += project.configurations.gwt
+            project.sourceSets.test.runtimeClasspath += project.configurations.gwt
+        }
+
+        if(!project.configurations.hasProperty("gwtSources")){
+            project.configurations.add('gwtSources')
+        }
+    }
 }
--- src/main/groovy/fi/jasoft/plugin/tasks/CreateServlet3ProjectTask.groovy
+++ src/main/groovy/fi/jasoft/plugin/tasks/CreateServlet3ProjectTask.groovy
@@ -15,7 +15,9 @@
 */
 package fi.jasoft.plugin.tasks;
 
-import org.gradle.api.DefaultTask;
+import org.gradle.api.DefaultTask
+import org.gradle.api.artifacts.ExternalModuleDependency
+import org.gradle.api.artifacts.ProjectDependency;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.plugins.WarPluginConvention;
 import org.gradle.api.plugins.JavaPluginConvention;
