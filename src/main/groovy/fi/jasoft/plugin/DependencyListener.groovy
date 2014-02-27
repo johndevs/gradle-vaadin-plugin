f2d43c4b34b72ea47107223c7a34e003e2153320
Paul Shomo <pshomo@copyright.com>
2014-02-27 12:37:03 -0500

    Added configuration flag to control adding repositories to project. #86

--- src/main/groovy/fi/jasoft/plugin/DependencyListener.groovy
+++ src/main/groovy/fi/jasoft/plugin/DependencyListener.groovy
@@ -69,8 +69,10 @@ class DependencyListener implements ProjectEvaluationListener {
             return
         }
 
-        // Repositories
-        addRepositories(project)
+        // Add repositories unless specified otherwise
+        if (project.vaadin.manageRepositories) {
+            addRepositories(project)
+        }
 
         createJetty8Configuration(project)
 
