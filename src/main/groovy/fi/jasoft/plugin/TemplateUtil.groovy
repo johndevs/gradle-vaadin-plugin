81e805b1e5e61d7896f0766a4937aa4464f525c6
John Ahlroos <john.ahlroos@jasoft.fi>
2014-04-17 11:00:44 +0300

    Added methods for creating templates with SimpleTemplateEngine and ported widgetset generation to it #98

--- src/main/groovy/fi/jasoft/plugin/TemplateUtil.groovy
+++ src/main/groovy/fi/jasoft/plugin/TemplateUtil.groovy
@@ -13,8 +13,9 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package fi.jasoft.plugin;
+package fi.jasoft.plugin
 
+import groovy.text.SimpleTemplateEngine;
 import org.gradle.api.Project
 import java.util.jar.JarEntry
 import java.util.jar.JarFile
@@ -24,6 +25,7 @@ import java.util.jar.Attributes
 
 class TemplateUtil {
 
+    @Deprecated
     protected static String getTemplateContent(String template) {
         if (template == null){
             throw new IllegalArgumentException("Template name cannot be null")
@@ -37,10 +39,12 @@ class TemplateUtil {
         return templateStream.getText()
     }
 
+    @Deprecated
     public static void writeTemplate(String template, File targetDir, Map substitutions) {
         writeTemplate(template, targetDir, template, substitutions)
     }
 
+    @Deprecated
     public static void writeTemplate(String template, File targetDir, String targetFileName=template, Map substitutions=[:], boolean removeBlankLines=false) {
         String content = TemplateUtil.getTemplateContent(template)
 
@@ -65,192 +69,31 @@ class TemplateUtil {
         targetFile.write(content)
     }
 
-    public static boolean ensureWidgetPresent(Project project) {
-        if (!project.vaadin.manageWidgetset) {
-            return false;
+    public static void writeTemplate2(String templateFileName, File targetDir, String targetFileName=templateFileName, Map substitutions=[:], boolean removeBlankLines=false) {
+        def templateUrl = TemplateUtil.class.getClassLoader().getResource("templates/${templateFileName}.template")
+        if (templateUrl == null) {
+            throw new FileNotFoundException("Could not find template 'templates/${templateFileName}.template'")
         }
 
-        // Check source dir if widgetset is present there
-        File javaDir = Util.getMainSourceSet(project).srcDirs.iterator().next()
-        File widgetsetFile = new File(javaDir.canonicalPath + '/' + project.vaadin.widgetset.replaceAll(/\./, '/') + ".gwt.xml")
+        def engine = new SimpleTemplateEngine()
+        def template = engine.createTemplate(templateUrl).make(substitutions.withDefault {null})
+        def content = template.toString()
 
-        if (widgetsetFile.exists()) {
-            updateWidgetset(widgetsetFile, project);
-            return false;
+        if (removeBlankLines){
+            content = content.replaceAll("(?m)^[ \t]*\r?\n", "")
         }
 
-        // Check resource dir if widgetset is present there
-        File resourceDir = project.sourceSets.main.resources.srcDirs.iterator().next()
-        widgetsetFile = new File(resourceDir.canonicalPath + '/' + project.vaadin.widgetset.replaceAll(/\./, '/') + ".gwt.xml")
-
-        if (widgetsetFile.exists()) {
-            updateWidgetset(widgetsetFile, project);
-            return false;
+        File targetFile = new File(targetDir.canonicalPath + '/' + targetFileName)
+        if(!targetFile.exists()){
+            targetFile.parentFile.mkdirs()
+            targetFile.createNewFile()
         }
 
-        // No widgetset detected, create one
-        new File(widgetsetFile.parent).mkdirs()
-
-        widgetsetFile.createNewFile()
-
-        updateWidgetset(widgetsetFile, project);
-
-        return true;
-    }
-
-    public static void updateWidgetset(File widgetsetFile, Project project) {
-        String inherits = ""
-
-        // Scan classpath for Vaadin addons and inherit their widgetsets
-        project.configurations.compile.each {
-            JarInputStream jarStream = new JarInputStream(it.newDataInputStream());
-            Manifest mf = jarStream.getManifest();
-            if (mf != null) {
-                Attributes attributes = mf.getMainAttributes()
-                if (attributes != null) {
-                    String widgetsets = attributes.getValue('Vaadin-Widgetsets')
-                    if (widgetsets != null) {
-                        for (String widgetset : widgetsets.split(",")) {
-                            if (widgetset != 'com.vaadin.terminal.gwt.DefaultWidgetSet'
-                                    && widgetset != 'com.vaadin.DefaultWidgetSet') {
-                                inherits += "\t<inherits name=\"${widgetset}\" />\n"
-                            }
-                        }
-                    }
-                }
-            }
+        if (!targetFile.canWrite()){
+            throw new FileNotFoundException("Could not write to target file "+targetFile.canonicalPath)
         }
 
-        // Custom inherits
-        if(project.vaadin.gwt.extraInherits != null){
-            for(String inherit : project.vaadin.gwt.extraInherits) {
-                inherits += "\t<inherits name=\"${inherit}\" />\n"
-            }
-        }
-
-        File widgetsetDir = new File(widgetsetFile.parent)
-
-        String moduleXML = project.vaadin.widgetset.tokenize('.').last() + ".gwt.xml"
-
-        String sourcePaths = ""
-        for (String path : project.vaadin.gwt.sourcePaths) {
-            sourcePaths += "\t<source path=\"${path}\" />\n"
-        }
-
-        def substitutions = [:]
-        substitutions['%INHERITS%'] = inherits
-        substitutions['%WIDGETSET%'] = project.vaadin.widgetset
-        substitutions['%SUPERDEVMODE%'] = String.valueOf(project.vaadin.devmode.superDevMode)
-
-        def ua = 'ie8,ie9,gecko1_8,safari,opera'
-        if(project.vaadin.gwt.userAgent == null){
-            if (Util.isIE10UserAgentSupported(project)){
-                 ua += ',ie10'
-            }
-        } else {
-            ua = project.vaadin.gwt.userAgent
-        }
-
-        substitutions['%USERAGENT%'] = ua
-        substitutions['%SOURCE%'] = sourcePaths
-        substitutions['%PROFILER_ENABLED%'] = project.vaadin.profiler ? 'true' : 'false'
-
-        String name, pkg, filename
-        if (project.vaadin.widgetsetGenerator == null) {
-            name = project.vaadin.widgetset.tokenize('.').last()
-            pkg = project.vaadin.widgetset.replaceAll('.' + name, '') + '.client.ui'
-            filename = name + "Generator.java"
-
-        } else {
-            name = project.vaadin.widgetsetGenerator.tokenize('.').last()
-            pkg = project.vaadin.widgetsetGenerator.replaceAll('.' + name, '')
-            filename = name + ".java"
-        }
-
-        File javaDir = Util.getMainSourceSet(project).srcDirs.iterator().next()
-        File f = new File(javaDir.canonicalPath + '/' + pkg.replaceAll(/\./, '/') + '/' + filename)
-
-        if (f.exists() || project.vaadin.widgetsetGenerator != null) {
-
-            String generatorString = "\t<generate-with class=\"${pkg}.${filename.replaceAll('.java', '')}\">\n" +
-                    "\t\t<when-type-assignable class=\"com.vaadin.client.metadata.ConnectorBundleLoader\" />\n" +
-                    "\t</generate-with>"
-
-            substitutions['%WIDGETSET_GENERATOR%'] = generatorString
-        } else {
-            substitutions['%WIDGETSET_GENERATOR%'] = ''
-        }
-
-        substitutions['%SASS_LINKER%'] = ''
-        substitutions['%STYLESHEETS%'] = ''
-
-        // Retrieve SCSS files
-        File[] clientSCSS = getFilesFromPublicFolder(project, 'scss')
-        if (clientSCSS.length > 0) {
-            String linkerString = "\t<define-linker name=\"scssintegration\" class=\"com.vaadin.sass.linker.SassLinker\" />\n" +
-                    "\t<add-linker name=\"scssintegration\" />";
-            substitutions['%SASS_LINKER%'] = linkerString
-
-            StringBuilder stylesheets = new StringBuilder(substitutions['%STYLESHEETS%'])
-            clientSCSS.each {
-
-                // Get the relative path to the public folder
-                String path = it.parent.substring(it.parent.lastIndexOf('public') + 'public'.length())
-                if (path.startsWith('/')) {
-                    path = path.substring(1);
-                }
-
-                // Convert file name from scss -> css
-                filename = it.name.substring(0, it.name.length() - 4) + 'css'
-
-                // Recreate relative path to scss file
-                path = path + '/' + filename
-                if (path.startsWith('/')) {
-                    path = path.substring(1);
-                }
-
-                stylesheets.append("\t<stylesheet src=\"${path}\"/>\n")
-            }
-
-            substitutions['%STYLESHEETS%'] = stylesheets.toString()
-        }
-
-        // Retrive CSS files
-        File[] clientCSS = getFilesFromPublicFolder(project, 'css')
-        if (clientCSS.length > 0) {
-            StringBuilder stylesheets = new StringBuilder(substitutions['%STYLESHEETS%'])
-            clientCSS.each {
-
-                // Get the relative path to the public folder
-                String path = it.parent.substring(it.parent.lastIndexOf('public') + 'public'.length())
-                if (path.startsWith('/')) {
-                    path = path.substring(1);
-                }
-
-                // Recreate relative path to css file
-                path = path + '/' + it.name
-                if (path.startsWith('/')) {
-                    path = path.substring(1);
-                }
-
-                stylesheets.append("\t<stylesheet src=\"${path}\"/>\n")
-            }
-            substitutions['%STYLESHEETS%'] = stylesheets.toString()
-        }
-
-        if (project.vaadin.gwt.collapsePermutations) {
-            substitutions['%COLLAPSE_PERMUTATIONS%'] = "\t<collapse-all-properties />"
-        } else {
-            substitutions['%COLLAPSE_PERMUTATIONS%'] = ''
-        }
-
-        if (project.vaadin.gwt.logging) {
-            substitutions['%LOGGING%'] = ''
-        } else {
-            substitutions['%LOGGING%'] = "\t<set-property name=\"gwt.logging.enabled\" value=\"FALSE\"/>"
-        }
-
-        TemplateUtil.writeTemplate('Widgetset.xml', widgetsetDir, moduleXML, substitutions,true)
+        targetFile.write(content)
     }
 
     /**
--- src/main/groovy/fi/jasoft/plugin/tasks/UpdateWidgetsetTask.groovy
+++ src/main/groovy/fi/jasoft/plugin/tasks/UpdateWidgetsetTask.groovy
@@ -13,11 +13,17 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package fi.jasoft.plugin.tasks;
+package fi.jasoft.plugin.tasks
 
+import fi.jasoft.plugin.Util;
 import org.gradle.api.DefaultTask
+import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
 import fi.jasoft.plugin.TemplateUtil
+
+import java.util.jar.Attributes
+import java.util.jar.JarInputStream
+import java.util.jar.Manifest
 
 class UpdateWidgetsetTask extends DefaultTask {
 
@@ -33,6 +39,198 @@ class UpdateWidgetsetTask extends DefaultTask {
             return;
         }
 
-        TemplateUtil.ensureWidgetPresent(project)
+        ensureWidgetPresent(project)
+    }
+
+    public static boolean ensureWidgetPresent(Project project) {
+        if (!project.vaadin.manageWidgetset) {
+            return false;
+        }
+
+        // Check source dir if widgetset is present there
+        File javaDir = Util.getMainSourceSet(project).srcDirs.iterator().next()
+        File widgetsetFile = new File(javaDir.canonicalPath + '/' + project.vaadin.widgetset.replaceAll(/\./, '/') + ".gwt.xml")
+
+        if (widgetsetFile.exists()) {
+            updateWidgetset(widgetsetFile, project);
+            return false;
+        }
+
+        // Check resource dir if widgetset is present there
+        File resourceDir = project.sourceSets.main.resources.srcDirs.iterator().next()
+        widgetsetFile = new File(resourceDir.canonicalPath + '/' + project.vaadin.widgetset.replaceAll(/\./, '/') + ".gwt.xml")
+
+        if (widgetsetFile.exists()) {
+            updateWidgetset(widgetsetFile, project);
+            return false;
+        }
+
+        // No widgetset detected, create one
+        new File(widgetsetFile.parent).mkdirs()
+
+        widgetsetFile.createNewFile()
+
+        updateWidgetset(widgetsetFile, project);
+
+        return true;
+    }
+
+    private static void updateWidgetset(File widgetsetFile, Project project) {
+        def substitutions = [:]
+
+        def inherits = ['com.vaadin.DefaultWidgetSet']
+
+        // Scan classpath for Vaadin addons and inherit their widgetsets
+        project.configurations.compile.each {
+            JarInputStream jarStream = new JarInputStream(it.newDataInputStream());
+            Manifest mf = jarStream.getManifest();
+            if (mf != null) {
+                Attributes attributes = mf.getMainAttributes()
+                if (attributes != null) {
+                    String widgetsets = attributes.getValue('Vaadin-Widgetsets')
+                    if (widgetsets != null) {
+                        for (String widgetset : widgetsets.split(",")) {
+                            if (widgetset != 'com.vaadin.terminal.gwt.DefaultWidgetSet'
+                                    && widgetset != 'com.vaadin.DefaultWidgetSet') {
+                                inherits.push(widgetset)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Custom inherits
+        if(project.vaadin.gwt.extraInherits != null){
+            inherits.addAll(project.vaadin.gwt.extraInherits)
+        }
+
+        substitutions['inherits'] = inherits
+
+        //###################################################################
+
+        substitutions['sourcePaths'] = project.vaadin.gwt.sourcePaths
+
+        //###################################################################
+
+        def configurationProperties = [:]
+        if (project.vaadin.devmode.superDevMode) {
+            configurationProperties['devModeRedirectEnabled'] = true
+        }
+
+        substitutions['configurationProperties'] = configurationProperties
+
+        //###################################################################
+
+        def properties =[:]
+
+        def ua = 'ie8,ie9,gecko1_8,safari,opera'
+        if(project.vaadin.gwt.userAgent == null){
+            if (Util.isIE10UserAgentSupported(project)){
+                ua += ',ie10'
+            }
+        } else {
+            ua = project.vaadin.gwt.userAgent
+        }
+        properties.put('user.agent', ua)
+
+        if (project.vaadin.profiler) {
+            properties.put('vaadin.profiler', true)
+        }
+
+        if (!project.vaadin.gwt.logging) {
+            properties.put('gwt.logging.enabled', false)
+        }
+
+        substitutions['properties'] = properties
+
+        //###################################################################
+
+        String name, pkg, filename
+        if (project.vaadin.widgetsetGenerator == null) {
+            name = project.vaadin.widgetset.tokenize('.').last()
+            pkg = project.vaadin.widgetset.replaceAll('.' + name, '') + '.client.ui'
+            filename = name + "Generator.java"
+
+        } else {
+            name = project.vaadin.widgetsetGenerator.tokenize('.').last()
+            pkg = project.vaadin.widgetsetGenerator.replaceAll('.' + name, '')
+            filename = name + ".java"
+        }
+
+        File javaDir = Util.getMainSourceSet(project).srcDirs.iterator().next()
+        File f = new File(javaDir.canonicalPath + '/' + pkg.replaceAll(/\./, '/') + '/' + filename)
+
+        if (f.exists() || project.vaadin.widgetsetGenerator != null) {
+            substitutions['widgetsetGenerator'] = pkg + filename.replaceAll('.java', '')
+        }
+
+        //###################################################################
+
+        def linkers = [:]
+
+        File[] clientSCSS = TemplateUtil.getFilesFromPublicFolder(project, 'scss')
+        if (clientSCSS.length > 0) {
+            linkers.put('scssintegration', 'com.vaadin.sass.linker.SassLinker')
+        }
+
+        substitutions['linkers'] = linkers
+
+        //###################################################################
+
+        def stylesheets = []
+
+        clientSCSS.each {
+
+            // Get the relative path to the public folder
+            String path = it.parent.substring(it.parent.lastIndexOf('public') + 'public'.length())
+            if (path.startsWith('/')) {
+                path = path.substring(1);
+            }
+
+            // Convert file name from scss -> css
+            filename = it.name.substring(0, it.name.length() - 4) + 'css'
+
+            // Recreate relative path to scss file
+            path = path + '/' + filename
+            if (path.startsWith('/')) {
+                path = path.substring(1);
+            }
+
+            stylesheets.add(path)
+        }
+
+        // Retrive CSS files
+        File[] clientCSS = TemplateUtil.getFilesFromPublicFolder(project, 'css')
+        clientCSS.each {
+
+            // Get the relative path to the public folder
+            String path = it.parent.substring(it.parent.lastIndexOf('public') + 'public'.length())
+            if (path.startsWith('/')) {
+                path = path.substring(1);
+            }
+
+            // Recreate relative path to css file
+            path = path + '/' + it.name
+            if (path.startsWith('/')) {
+                path = path.substring(1);
+            }
+
+            stylesheets.add(path)
+        }
+
+        substitutions['stylesheets'] = stylesheets
+
+        //###################################################################
+
+        substitutions['collapsePermutations'] = project.vaadin.gwt.collapsePermutations
+
+        //###################################################################
+
+
+        // Write widgetset file
+        File widgetsetDir = new File(widgetsetFile.parent)
+        String moduleXML = project.vaadin.widgetset.tokenize('.').last() + ".gwt.xml"
+        TemplateUtil.writeTemplate2('Widgetset.xml', widgetsetDir, moduleXML, substitutions, true)
     }
 }
