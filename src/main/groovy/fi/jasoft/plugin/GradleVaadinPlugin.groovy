c6e1d75357256dd6eb67df336a551723e4224721
John Ahlroos <john.ahlroos@jasoft.fi>
2015-08-06 20:17:13 +0300

    Require Gradle 2.6 #195

--- src/main/groovy/fi/jasoft/plugin/GradleVaadinPlugin.groovy
+++ src/main/groovy/fi/jasoft/plugin/GradleVaadinPlugin.groovy
@@ -19,8 +19,12 @@ import fi.jasoft.plugin.configuration.VaadinPluginExtension
 import fi.jasoft.plugin.tasks.*
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.dsl.ArtifactHandler
+import org.gradle.api.invocation.Gradle
 import org.gradle.api.plugins.WarPlugin
+import org.gradle.api.tasks.TaskContainer
 import org.gradle.tooling.UnsupportedVersionException
+import org.gradle.util.VersionNumber
 
 class GradleVaadinPlugin implements Plugin<Project> {
 
@@ -55,79 +59,86 @@ class GradleVaadinPlugin implements Plugin<Project> {
 
     void apply(Project project) {
 
-        if(!project.gradle.gradleVersion.startsWith("2")) {
-            throw new UnsupportedVersionException("Your gradle version ("+project.gradle.gradleVersion+") is too old. Plugin requires Gradle 2.0+")
+        def gradle = project.gradle
+        def version = VersionNumber.parse(gradle.gradleVersion)
+        def requiredVersion = new VersionNumber(2, 6, 0, null)
+        if(version.baseVersion < requiredVersion) {
+            throw new UnsupportedVersionException("Your gradle version ($version) is too old. Plugin requires Gradle $requiredVersion+")
         }
 
         PLUGINS_IN_PROJECT++;
 
-        if (isFirstPlugin()) {
+        if (firstPlugin) {
             project.logger.quiet("Using Gradle Vaadin Plugin " + PLUGIN_VERSION)
         }
-
 
         // Extensions
         project.extensions.create('vaadin', VaadinPluginExtension)
 
         // Dependency resolution
-        project.getGradle().addProjectEvaluationListener(new DependencyListener())
-        project.getGradle().getTaskGraph().addTaskExecutionListener(new TaskListener(project))
+        gradle.addProjectEvaluationListener(new DependencyListener())
+        gradle.taskGraph.addTaskExecutionListener(new TaskListener(project))
 
         // Plugins
         project.plugins.apply(WarPlugin)
 
         // Tasks
-        project.tasks.create(name: CreateProjectTask.NAME, type: CreateProjectTask, group: 'Vaadin')
-        project.tasks.create(name: CreateComponentTask.NAME, type: CreateComponentTask, group: 'Vaadin')
-        project.tasks.create(name: CreateCompositeTask.NAME, type: CreateCompositeTask, group: 'Vaadin')
-        project.tasks.create(name: CreateThemeTask.NAME, type: CreateThemeTask, group: 'Vaadin')
-        project.tasks.create(name: CreateWidgetsetGeneratorTask.NAME, type: CreateWidgetsetGeneratorTask, group: 'Vaadin')
+        def tasks = project.tasks
+        tasks.create(name: CreateProjectTask.NAME, type: CreateProjectTask, group: 'Vaadin')
+        tasks.create(name: CreateComponentTask.NAME, type: CreateComponentTask, group: 'Vaadin')
+        tasks.create(name: CreateCompositeTask.NAME, type: CreateCompositeTask, group: 'Vaadin')
+        tasks.create(name: CreateThemeTask.NAME, type: CreateThemeTask, group: 'Vaadin')
+        tasks.create(name: CreateWidgetsetGeneratorTask.NAME, type: CreateWidgetsetGeneratorTask, group: 'Vaadin')
 
-        project.tasks.create(name: CompileWidgetsetTask.NAME, type: CompileWidgetsetTask, group: 'Vaadin')
-        project.tasks.create(name: DevModeTask.NAME, type: DevModeTask, group: 'Vaadin')
-        project.tasks.create(name: SuperDevModeTask.NAME, type: SuperDevModeTask, group: 'Vaadin')
-        project.tasks.create(name: CompileThemeTask.NAME, type: CompileThemeTask, group: 'Vaadin')
-        project.tasks.create(name: RunTask.NAME, type: RunTask, group: 'Vaadin')
-        project.tasks.create(name: UpdateWidgetsetTask.NAME, type: UpdateWidgetsetTask, group: 'Vaadin')
+        tasks.create(name: CompileWidgetsetTask.NAME, type: CompileWidgetsetTask, group: 'Vaadin')
+        tasks.create(name: DevModeTask.NAME, type: DevModeTask, group: 'Vaadin')
+        tasks.create(name: SuperDevModeTask.NAME, type: SuperDevModeTask, group: 'Vaadin')
+        tasks.create(name: CompileThemeTask.NAME, type: CompileThemeTask, group: 'Vaadin')
+        tasks.create(name: RunTask.NAME, type: RunTask, group: 'Vaadin')
+        tasks.create(name: UpdateWidgetsetTask.NAME, type: UpdateWidgetsetTask, group: 'Vaadin')
 
-        project.tasks.create(name: UpdateAddonStylesTask.NAME, type: UpdateAddonStylesTask, group: 'Vaadin')
-        project.tasks.create(name: CreateAddonThemeTask.NAME, type: CreateAddonThemeTask, group: 'Vaadin')
+        tasks.create(name: UpdateAddonStylesTask.NAME, type: UpdateAddonStylesTask, group: 'Vaadin')
+        tasks.create(name: CreateAddonThemeTask.NAME, type: CreateAddonThemeTask, group: 'Vaadin')
 
-        project.tasks.create(name: BuildSourcesJarTask.NAME, type: BuildSourcesJarTask, group: 'Vaadin Utility')
-        project.tasks.create(name: BuildJavadocJarTask.NAME, type: BuildJavadocJarTask, group: 'Vaadin Utility')
-        project.tasks.create(name: BuildClassPathJar.NAME, type: BuildClassPathJar, group: 'Vaadin Utility')
+        tasks.create(name: BuildSourcesJarTask.NAME, type: BuildSourcesJarTask, group: 'Vaadin Utility')
+        tasks.create(name: BuildJavadocJarTask.NAME, type: BuildJavadocJarTask, group: 'Vaadin Utility')
+        tasks.create(name: BuildClassPathJar.NAME, type: BuildClassPathJar, group: 'Vaadin Utility')
 
-        project.tasks.create(name: CreateTestbenchTestTask.NAME, type: CreateTestbenchTestTask, group: 'Vaadin Testbench')
+        tasks.create(name: CreateTestbenchTestTask.NAME, type: CreateTestbenchTestTask, group: 'Vaadin Testbench')
 
-        project.tasks.create(name: DirectorySearchTask.NAME, type: DirectorySearchTask, group: 'Vaadin Directory')
-        project.tasks.create(name: CreateDirectoryZipTask.NAME, type: CreateDirectoryZipTask, group: 'Vaadin Directory')
+        tasks.create(name: DirectorySearchTask.NAME, type: DirectorySearchTask, group: 'Vaadin Directory')
+        tasks.create(name: CreateDirectoryZipTask.NAME, type: CreateDirectoryZipTask, group: 'Vaadin Directory')
 
         // Add debug information to all compilation results
-        project.tasks.compileJava.options.debugOptions.debugLevel = 'source,lines,vars'
+        tasks.compileJava.options.debugOptions.debugLevel = 'source,lines,vars'
 
         // Add sources to test classpath
         project.sourceSets.test.runtimeClasspath += [project.files(project.sourceSets.main.java.srcDirs)]
 
         // War project should build the widgetset and themes
-        project.war.dependsOn(CompileWidgetsetTask.NAME)
-        project.war.dependsOn(CompileThemeTask.NAME)
+        def war = project.war
+        war.dependsOn(CompileWidgetsetTask.NAME)
+        war.dependsOn(CompileThemeTask.NAME)
 
         // Ensure widgetset is up-2-date
-        project.processResources.dependsOn(UpdateWidgetsetTask.NAME)
+        def resources = project.processResources
+        resources.dependsOn(UpdateWidgetsetTask.NAME)
 
         // Ensure addon themes are up2date
-        project.processResources.dependsOn(UpdateAddonStylesTask.NAME)
+        resources.dependsOn(UpdateAddonStylesTask.NAME)
 
         // Cleanup plugin outputs
-        project.clean.dependsOn(project.tasks['clean' + CompileWidgetsetTask.NAME.capitalize()])
-        project.clean.dependsOn(project.tasks['clean' + RunTask.NAME.capitalize()])
-        project.clean.dependsOn(project.tasks['clean' + CompileThemeTask.NAME.capitalize()])
-        project.clean.dependsOn(project.tasks['clean' + SuperDevModeTask.NAME.capitalize()])
-        project.clean.dependsOn(project.tasks['clean' + DevModeTask.NAME.capitalize()])
+        def clean = project.clean
+        clean.dependsOn(tasks['clean' + CompileWidgetsetTask.NAME.capitalize()])
+        clean.dependsOn(tasks['clean' + RunTask.NAME.capitalize()])
+        clean.dependsOn(tasks['clean' + CompileThemeTask.NAME.capitalize()])
+        clean.dependsOn(tasks['clean' + SuperDevModeTask.NAME.capitalize()])
+        clean.dependsOn(tasks['clean' + DevModeTask.NAME.capitalize()])
 
         // Utilities
-        project.artifacts.add('archives', project.tasks[BuildSourcesJarTask.NAME])
-        project.artifacts.add('archives', project.tasks[BuildJavadocJarTask.NAME])
+        def artifacts = project.artifacts
+        artifacts.add('archives', tasks[BuildSourcesJarTask.NAME])
+        artifacts.add('archives', tasks[BuildJavadocJarTask.NAME])
     }
 
 }
