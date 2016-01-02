3dd532b959a5b2df200608fafe01f72e5e043844
John Ahlroos <john.ahlroos@jasoft.fi>
2016-01-02 13:12:11 +0200

    Update jetty to latest 9.3 version

--- src/main/groovy/fi/jasoft/plugin/servers/JettyApplicationServer.groovy
+++ src/main/groovy/fi/jasoft/plugin/servers/JettyApplicationServer.groovy
@@ -1,16 +1,31 @@
+/*
+* Copyright 2016 John Ahlroos
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package fi.jasoft.plugin.servers
 
 import org.gradle.api.artifacts.DependencySet
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
 /**
- * Created by john on 1.1.2016.
+ * Runs the project on a Jetty server
  */
 class JettyApplicationServer extends ApplicationServer {
 
     public static final String NAME = 'jetty'
 
-    def jettyVersion = '9.3.0.v20150612'
+    def jettyVersion = '9.3.6.v20151106'
 
     JettyApplicationServer(Object project, Object browserParameters) {
         super(project, browserParameters)
