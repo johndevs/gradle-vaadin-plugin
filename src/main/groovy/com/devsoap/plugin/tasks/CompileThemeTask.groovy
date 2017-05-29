be89d61b3c0dac3ee77082bdd2b875522b2bee5a
neher <wimpieke@gmail.com>
2017-05-29 08:55:29 +0200

    Unpack everything

--- src/main/groovy/com/devsoap/plugin/tasks/CompileThemeTask.groovy
+++ src/main/groovy/com/devsoap/plugin/tasks/CompileThemeTask.groovy
@@ -264,7 +264,7 @@ class CompileThemeTask extends DefaultTask {
                                         includeEmptyDirs = false
                                         from project.zipTree(file)
                                         into unpackedVaadinDir
-                                        include 'VAADIN/themes/**/*.scss', 'VAADIN/addons/**/*.scss'
+                                        include 'VAADIN/themes/**/*', 'VAADIN/addons/**/*'
                                         eachFile { details ->
                                             details.path -= 'VAADIN'
                                         }
