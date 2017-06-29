cde36f088e59750772f65c9bc4316a115a9527c7
John Ahlroos <john@ahlroos.me>
2017-06-29 08:46:19 +0300

    Fix javadoc issues

--- src/main/java/com/devsoap/plugin/JettyServerRunner.java
+++ src/main/java/com/devsoap/plugin/JettyServerRunner.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2017 John Ahlroos
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2017 John Ahlroos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.devsoap.plugin;
 
 import org.eclipse.jetty.annotations.AnnotationConfiguration;
@@ -36,6 +36,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
+/**
+ * Runner for Jetty
+ *
+ * @author John Ahlroos
+ */
 public class JettyServerRunner {
 
     public static final String SERVER_STARTING_TOKEN = "Jetty starting";
--- src/main/java/com/devsoap/plugin/LibSassCompiler.java
+++ src/main/java/com/devsoap/plugin/LibSassCompiler.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2017 John Ahlroos
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2017 John Ahlroos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.devsoap.plugin;
 
 import io.bit3.jsass.CompilationException;
@@ -25,6 +25,11 @@ import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 
+/**
+ * Runner for Libsass compiler
+ *
+ * @author John Ahlroos
+ */
 public class LibSassCompiler {
 
     // Usage: 'LibSassCompiler [scss] [css] [unpackedThemes]
