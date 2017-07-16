60133c4a4387a12bc74fa3d6f9020679109f2b9b
John Ahlroos <john@ahlroos.me>
2017-07-16 17:21:54 +0300

    Add basic Kotlin DSL task configuration test

--- src/test/groovy/com/devsoap/plugin/integration/KotlinTest.groovy
+++ src/test/groovy/com/devsoap/plugin/integration/KotlinTest.groovy
@@ -1,7 +1,10 @@
 package com.devsoap.plugin.integration
 
 import com.devsoap.plugin.tasks.CreateProjectTask
+import com.devsoap.plugin.tasks.RunTask
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
 import java.nio.file.Paths
 
@@ -10,10 +13,16 @@ import static org.junit.Assert.assertTrue
 /**
  * Tests Kotlin project creation and usage
  */
+@RunWith(Parameterized)
 class KotlinTest extends KotlinIntegrationTest {
 
     KotlinTest(String kotlinVersion) {
         super(kotlinVersion)
+    }
+
+    @Parameterized.Parameters(name = "Kotlin {0}")
+    static Collection<String> getKotlinVersions() {
+        [ '1.1.3-2']
     }
 
     @Test void 'Create project'() {
@@ -33,6 +42,19 @@ class KotlinTest extends KotlinIntegrationTest {
         assertTrue 'Classes should exist', classes.exists()
         assertTrue 'Servlet not compiled', new File(classes, 'HelloWorldServlet.class').exists()
         assertTrue 'UI not compiled', new File(classes, 'HelloWorldUI.class').exists()
+    }
+
+    @Test void 'Run with Jetty'() {
+        buildFile << """
+           val vaadinRun : com.devsoap.plugin.tasks.RunTask by tasks
+           vaadinRun.apply {
+                server = "jetty"
+           }
+        """.stripIndent()
+
+        def output = runWithArguments('--info', CreateProjectTask.NAME, RunTask.NAME, '--stopAfterStart')
+        assertTrue output, output.contains("Starting jetty")
+        assertTrue output, output.contains('Application running on ')
     }
 }
 
